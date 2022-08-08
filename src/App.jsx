88459965bf65846dfea67a4488a8fd2fc554c5cb
alfianahar <alfian.aswinda@gmail.com>
2022-08-08 20:04:00 +0700

    1 binaries uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,55 @@
-import { useEffect, useState } from 'react'
-
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [counter, setCounter] = useState(60);
-  const [play, setPlay] = useState(false)
-
-  useEffect(() => {
-    const timer = 
-      counter > 0 && play === true && setInterval(() => setCounter(counter - 1), 1000);
-      return () => clearInterval(timer)
-  }, [play, counter])
-
+function PomoSetting () {
   return (
     <div>
-      <span>Countdown: {counter}</span> <br/>
-      <button onClick={() => setPlay(true)} >play</button>
+      
+    </div>
+  )
+}
+
+function Pomodoro() {
+  return (
+    <div className="pomodoro">
+      <PomoSetting />
+    </div>
+  )
+}
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Pomodoro Timer</h1>
+      <Pomodoro />
+
+
+
+
+
+
+      <footer className="credit">
+        <p className="">
+          Made by{' '}
+          <span className="">
+            <a href="https://www.alfianahar.com/bio" target="_blank">
+              Alfian Nahar
+            </a>
+          </span>
+        </p>
+      </footer>
+      {/* <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div> */}
+      
     </div>
   )
 }
